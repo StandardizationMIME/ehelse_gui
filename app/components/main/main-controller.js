a583eabb8a20139d1c38cae1019f267f919084bb
Dag Frode Solberg <tncditto@gmail.com>
2016-03-05 13:12:12 +0100

    Split functionality out of TopicController into additional controllers EditorController and Content Controller

--- app/components/main/main-controller.js
+++ app/components/main/main-controller.js
@@ -80,69 +80,9 @@ function generateTopicList(parent, topics){
     }]);
 
 
-
-
-    // Controller for selecting a topic.
-    angular.module('ehelseEditor').controller('TopicController', function($scope, $http){
-
-        $scope.topic = {
-            title: "Referansekatalogen"
-        };
-
-        $scope.get('topics/' , function(data){
-            $scope.topics = data.topics;
-            $scope.topicList = generateTopicList("", data.topics);
-            console.log($scope.topics);
-
-        }, function(){});
-
-        $scope.getStandards = function(id) {
-            $scope.get('topics/' + id , function(data){
-                $scope.standards = data.documents;
-                $scope.topic = data;
-
-            }, function(){});
-
-            <!-- Makes selected folder bold and toggles folder icon between opened and closed -->
-            $(".clickable").removeClass('selected');
-            $('#' + id).addClass('selected');
-            $('#folder' + id).toggleClass('glyphicon-folder-open','glyphicon-folder-close');
-        };
-
-        $scope.changeView = function(view) {
-            $scope.view = view;
-        };
-
-        $scope.cancelContentBrowser = function(){
-            $scope.changeView("");
-        };
-
-        $scope.topicTitle = "Initial topicTitle value";
-
-
-        $scope.postNewTopic = function(){
-            console.log("Button was clicked");
-
-            console.log($scope.topicTitle);
-            //var data = $.param({
-            //    json: JSON.stringify({
-            //        title: $scope.topicTitle,
-            //        description: $scope.topicDescription,
-            //        number: "",
-            //        isInCatalog: $scope.topicIsInCatalog,
-            //        sequence: "",
-            //        parent: $scope.topicParent
-            //    })
-            //});
-            //$scope.post("", data).success(function(data, status) {
-            //    $scope.hello = data;
-            //});
-        }
+    angular.module('ehelseEditor').controller('TopicController', function($scope, $http) {
 
     });
-
-
-
 
 
 
