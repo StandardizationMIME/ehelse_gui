4a23b27526496857aae0db9cdc470898ac818e8b
Dag Frode Solberg <tncditto@gmail.com>
2016-03-05 12:56:29 +0100

    Removed unused service. Cleaned up file

--- app/components/login/login-controller.js
+++ app/components/login/login-controller.js
@@ -1,36 +1,21 @@
-/**
- * Created by ARAGORN on 29.02.2016.
- */
-
 'use strict';
 
 angular.module('ehelseEditor')
 
-.controller('LoginController',
-    ['$scope', '$rootScope', '$location', '$http',
-        function ($scope, $rootScope, $location, $http, AuthenticationService) {
+    .controller('LoginController',
+        ['$scope', '$rootScope', '$location', '$http',
+            function ($scope, $rootScope, $location, $http, AuthenticationService) {
 
-            $scope.login = function () {
-                console.log('login');
-                $scope.dataLoading = true;
-                $http.defaults.headers.common['Authorization'] = 'Basic ' +  $scope.username + ':' + $scope.password;
-                console.log($http.defaults.headers.common['Authorization']);
+                $scope.login = function () {
+                    console.log('login');
+                    $scope.dataLoading = true;
+                    $http.defaults.headers.common['Authorization'] = 'Basic ' +  $scope.username + ':' + $scope.password;
+                    console.log($http.defaults.headers.common['Authorization']);
 
-                $http.post('http://refkat.eu/v1/topics/', {username : username, password: password})
-                    .success(function(response){
-                        alert(response);
-                    });
+                    $http.post('http://refkat.eu/v1/topics/', {username : username, password: password})
+                        .success(function(response){
+                            alert(response);
+                        });
 
-                //AuthenticationService.Login($scope.username, $scope.password, function (response) {
-                //    if (response.success) {
-                //        AuthenticationService.SetCredentials($scope.username, $scope.password);
-                //        $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
-                //        console.log($http.defaults.headers.common['Authorization']);
-                //        $location.path('/');
-                //    } else {
-                //        $scope.error = response.message;
-                //        $scope.dataLoading = false;
-                //    }
-                //});
-            };
-        }]);
+                };
+            }]);
