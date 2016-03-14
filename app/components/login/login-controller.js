c9edac24a2e0a3096fe3ec70f62b1d2b0735a863
Marius Lundbø <marius.lundbo@gmail.com>
2016-03-14 13:11:11 +0100

    facelift + feedback fix

--- app/components/login/login-controller.js
+++ app/components/login/login-controller.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('ehelseEditor').controller('LoginController', [ '$scope', '$rootScope', '$location',  function( $scope, $rootScope, $location) {
+
     $scope.submit = function(){
         $rootScope.setUserName($scope.username);
         $rootScope.setPassword($scope.password);
@@ -15,6 +16,8 @@ angular.module('ehelseEditor').controller('LoginController', [ '$scope', '$rootS
                 console.log(data);
                 if (data.authenticated === true){
                     $location.path('/main-view').replace();
+                }else{
+                    $scope.password = '';
                 }
             },
             function(){}
