f9deb5caaac52d0ed625b83fc4dcf0bcecbf7e03
Marius Lundbø <marius.lundbo@gmail.com>
2016-03-17 13:55:44 +0100

    createdDropdown

--- app/app-routes.js
+++ app/app-routes.js
@@ -9,5 +9,17 @@ angular.module('ehelseEditor')
                 controller: 'MainController',
                 templateUrl: 'app/components/main/views/main-view.html'
             })
+            .when('/myPage-view/', {
+                controller: 'MyPageController',
+                templateUrl: 'app/components/main/views/main-view.html'
+            })
+            .when('/settings-view/', {
+                controller: 'SettingsController',
+                templateUrl: 'app/components/main/views/main-view.html'
+            })
+            .when('/administerUsers-view/', {
+                controller: 'AdministerUsersController',
+                templateUrl: 'app/components/main/views/main-view.html'
+            })
             .otherwise({redirectTo: '/'})
     }]);
