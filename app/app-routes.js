00a32fd7ed1b9008c9be3f4e26e4b43e4c94c4b8
Stian Dysthe <dysthe_stian@hotmail.com>
2016-03-17 22:33:03 +0100

    Removed unnecessary routes

--- app/app-routes.js
+++ app/app-routes.js
@@ -9,17 +9,5 @@ angular.module('ehelseEditor')
                 controller: 'MainController',
                 templateUrl: 'app/components/main/views/main-view.html'
             })
-            .when('/myPage-view/', {
-                controller: 'MyPageController',
-                templateUrl: 'app/components/main/views/main-view.html'
-            })
-            .when('/settings-view/', {
-                controller: 'SettingsController',
-                templateUrl: 'app/components/main/views/main-view.html'
-            })
-            .when('/administerUsers-view/', {
-                controller: 'AdministerUsersController',
-                templateUrl: 'app/components/main/views/main-view.html'
-            })
             .otherwise({redirectTo: '/'})
     }]);
