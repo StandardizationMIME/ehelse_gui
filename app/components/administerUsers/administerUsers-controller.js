7739a4cc0118dcec3f3b219c62a4b50a48404b97
Marius Lundbø <marius.lundbo@gmail.com>
2016-04-02 23:53:04 +0200

    ng-repeat and added style

--- app/components/administerUsers/administerUsers-controller.js
+++ app/components/administerUsers/administerUsers-controller.js
@@ -2,5 +2,9 @@
 
 angular.module('ehelseEditor').controller('AdministerUsersController', ['$scope', '$rootScope', function ($scope, $rootScope) {
 
+    $rootScope.get("/users/", function (data) {
+        $scope.userData = data;
+    }, function () {
+    });
 
 }]);
