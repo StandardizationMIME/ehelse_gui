ca878905e488c2f8eb4ea9de701b2a76e83fb923
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-06-10 12:32:16 +0200

    Added ServiceFunctions with getTimestamp()

---
+++ app/services/ServiceFunction.js
@@ -0,0 +1,41 @@
+"use strict";
+
+angular.module("ehelseEditor").factory("ServiceFunction", ["$rootScope", function($rootScope) {
+
+    /**
+     * Returns timestamp on database format
+     * @returns {string}
+     */
+    function getTimestamp() {
+        var time = new Date();
+        var year = time.getFullYear();
+        var month =  time.getMonth()+1; // Months are zero-based
+        var day = time.getDate();
+        var hours = time.getHours();
+        var minutes = time.getMinutes();
+        var seconds = time.getSeconds();
+
+        return year + "-" +
+            getTwoDigitNumber(month) + "-" +
+            getTwoDigitNumber(day) + " " +
+            getTwoDigitNumber(hours) + ":" +
+            getTwoDigitNumber(minutes) + ":" +
+            getTwoDigitNumber(seconds);
+    }
+
+    /**
+     * Returns zero in front of numbers less than 10: "05" instead of "5"
+     * @param number
+     * @returns {string}
+     */
+    function getTwoDigitNumber(number) {
+        // TODO: handle 3 digit numbers, give error?
+        return (number > 9 ? number : "0" + number);
+    }
+
+    return {
+        getTimestamp: getTimestamp
+    }
+
+
+}]);
