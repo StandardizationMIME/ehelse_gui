e0818d05aa736fb90a39afcc294cb684a2f4e6f0
Vitaly Semichev <vitaly.semich@gmail.com>
2016-06-10 10:19:36 +0200

    loading and save can be accessed from any ctrl

--- app/components/uploadFile/uploadFileController.js
+++ app/components/uploadFile/uploadFileController.js
@@ -1,60 +1,32 @@
 "use strict";
 
-angular.module("ehelseEditor").controller("UploadFileController", ["$scope","$rootScope", function($scope,$rootScope){
+angular.module("ehelseEditor").controller("UploadFileController", ["$scope", "$rootScope", "FileUpload", "StorageHandler", function($scope, $rootScope, FileUpload, StorageHandler){
 
-    $rootScope.json_object_form_file = {};
 
-    function isValidJson(json) {
-        try {
-            JSON.parse(json);
-        } catch (e) {
-            return false;
-        }
-        return true;
-    }
-
-    $scope.uploadFile = function(){
-
-        var f = document.getElementById("input-file").files[0];
-
-        if (f){
-            var reader = new FileReader();
-
-            reader.onload = function(e){
-                var json_text = e.target.result;
-                console.log(json_text);
-
-                if (isValidJson(json_text)){
-                    var json_object = JSON.parse(json_text);
-                    console.log(json_object);
-                }else{
-                    alert("invalid JSON");
-                }
-
-                angular.copy(json_object, $rootScope.json_object_form_file);
-            };
-            reader.readAsText(f);
-        }else{
-            alert("Failed to load file");
-        }
-
+    $scope.readFileContent = function ($fileContent) {
+        FileUpload.readContent($fileContent);
     };
-
-    $rootScope.saveTest = function () {
-        $rootScope.saveToFile($rootScope.json_object_form_file);
-    };
-
-
-    $rootScope.saveToFile = function (JSON_object) {
-        var blob = new Blob([JSON.stringify(JSON_object, null, '\t')], {type: "application/json"});
-        saveAs(blob, "your_shitty_json.json");
-    };
-
-
-    $rootScope.getJsonFile = function () {
-        console.log($rootScope.json_object_form_file);
-        return $rootScope.json_object_form_file;
-    }
-
 
 }]);
+
+
+angular.module("ehelseEditor").directive('onReadFile', function ($parse) {
+    return {
+        restrict: 'A',
+        scope: false,
+        link: function(scope, element, attrs){
+            var fn = $parse(attrs.onReadFile);
+
+            element.on('change', function (onChangeEvent) {
+                var reader = new FileReader();
+
+                reader.onload = function (onLoadEvent) {
+                    scope.$apply(function () {
+                        fn(scope, {$fileContent:onLoadEvent.target.result});
+                    });
+                };
+                reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+            });
+        }
+    };
+})
--- app/services/StorageHandler.js
+++ app/services/StorageHandler.js
@@ -1,8 +1,10 @@
 "use strict";
 
-angular.module("ehelseEditor").factory("StorageHandler", ["$rootScope", function ($rootScope) {
+angular.module("ehelseEditor").factory("StorageHandler", ["$rootScope", "FileUpload", function ($rootScope, FileUpload) {
 
-    var input_list; // TODO: Must be set from input file.
+    var input_list = FileUpload.getJsonFile();;
+    console.log(input_list);
+
     var SORT_ON_SEQUENCE = function (a, b) { // Constant for sequence sort (ascending)
         return a.sequence - b.sequence;
     };
