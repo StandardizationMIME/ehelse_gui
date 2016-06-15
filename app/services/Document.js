eed922a2fc8ea906ce3d5d22cbdf7a4063d90d95
Stian Dysthe <dysthe_stian@hotmail.com>
2016-06-15 12:24:45 +0200

    Fixed linking between versions when deleting the one in the middle

--- app/services/Document.js
+++ app/services/Document.js
@@ -175,7 +175,7 @@ angular.module("ehelseEditor").factory("Document", ["$rootScope", "DocumentField
             document.populatedProfiles = [];
             document.profiles = [];
             document.createdTimestamp = ServiceFunction.getTimestamp();
-            document.editedTimestamp = null;
+            document.editedTimestamp = ServiceFunction.getTimestamp();
         }else{
             console.log("Input = " + document + " and is invalid");
         }
@@ -209,9 +209,7 @@ angular.module("ehelseEditor").factory("Document", ["$rootScope", "DocumentField
             try{
                 //Clones current document and initialize it's values
                 var new_document = clone(current_document);
-                console.log(new_document);
                 initNewDocument(new_document);
-                console.log(new_document);
 
                 //push profile id to standard
                 if(new_document.standardId){
@@ -262,15 +260,22 @@ angular.module("ehelseEditor").factory("Document", ["$rootScope", "DocumentField
      * Sets next and previous documentID of the next and previous documents to null
      */
     function nullifyPreviousAndNextDocumentIdValues(){
-        if(documents_dict[current_document.previousDocumentId]){
-            documents_dict[current_document.previousDocumentId].nextDocumentId = null;
+        if(documents_dict[current_document.previousDocumentId] && documents_dict[current_document.nextDocumentId]){
+            documents_dict[current_document.previousDocumentId].nextDocumentId = documents_dict[current_document.nextDocumentId].id;
+            documents_dict[current_document.nextDocumentId].previousDocumentId = documents_dict[current_document.previousDocumentId].id;
             documents_dict[current_document.previousDocumentId].populatedProfiles = [];
-            updateDocumentInDocumentsList(documents_dict[current_document.previousDocumentId]);
-        }
-        if(documents_dict[current_document.nextDocumentId]){
-            documents_dict[current_document.nextDocumentId].previousDocumentId = null;
             documents_dict[current_document.nextDocumentId].populatedProfiles = [];
-            updateDocumentInDocumentsList(documents_dict[current_document.nextDocumentId]);
+        }else{
+            if(documents_dict[current_document.previousDocumentId]){
+                documents_dict[current_document.previousDocumentId].nextDocumentId = null;
+                documents_dict[current_document.previousDocumentId].populatedProfiles = [];
+                updateDocumentInDocumentsList(documents_dict[current_document.previousDocumentId]);
+            }
+            if(documents_dict[current_document.nextDocumentId]){
+                documents_dict[current_document.nextDocumentId].previousDocumentId = null;
+                documents_dict[current_document.nextDocumentId].populatedProfiles = [];
+                updateDocumentInDocumentsList(documents_dict[current_document.nextDocumentId]);
+            }
         }
     }
 
