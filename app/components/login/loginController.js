0cc1912c5d35a560287f3ba987ebe85e5c52eeae
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-06-15 15:53:05 +0200

    Deactivated go to editor buttong if no file is selected

--- app/components/login/loginController.js
+++ app/components/login/loginController.js
@@ -3,14 +3,21 @@
 angular.module("ehelseEditor").controller("LoginController", [ "$scope", "$rootScope", "$state", "FileUpload", function( $scope, $rootScope, $state, FileUpload) {
 
     $scope.$state = $state;
+    $scope.isLoaded = false;
 
     // Read file in file upload input
     $scope.readFileContent = function ($fileContent) {
         FileUpload.readContent($fileContent);
+        $scope.isLoaded = true;
     };
 
     $scope.goToSite = function(){
         $scope.$state.go("main-view.editor-view");
     }
+
+    // Check if file is uploaded
+    $scope.invalid = function () {
+        return !$scope.isLoaded;
+    };
 
 }]);
