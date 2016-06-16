9eec794f5988353cbd0f2322ec1958be799d65f5
Stian Dysthe <dysthe_stian@hotmail.com>
2016-06-16 11:06:07 +0200

    Added missing semi-colon

--- app/components/content/editDocument/editDocumentController.js
+++ app/components/content/editDocument/editDocumentController.js
@@ -34,7 +34,7 @@ angular.module("ehelseEditor").controller("EditDocumentController",
                     return {date: timestamp.substring(8,10) + "." + timestamp.substring(5,7) + "." + timestamp.substring(0,4), time: timestamp.substring(11,16)};
                 }
             };
-
+            
             // Delete selected document
             $scope.deleteDocument = function(){
                 Document.deleteCurrentDocument();
@@ -68,7 +68,7 @@ angular.module("ehelseEditor").controller("EditDocumentController",
                 Document.setCurrentDocument(Document.newVersion(document));
                 $rootScope.notifySuccess("Ny versjon klargjort", 3000);
                 $rootScope.setDocumentState('newDocument');
-            }
+            };
 
             function generateXMLString(name, content){
                 var resultXML;
