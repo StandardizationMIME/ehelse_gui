a7cc91a1ebb1ec27262e246cd4c44861c75aa681
Vitaly Semichev <vitaly.semich@gmail.com>
2016-06-17 14:46:47 +0200

    LinkCategories done

--- app/components/content/administerLinkCategories/administerLinkCategoriesController.js
+++ app/components/content/administerLinkCategories/administerLinkCategoriesController.js
@@ -19,4 +19,23 @@ angular.module("ehelseEditor").controller("AdministerLinkCategoriesController",[
         $rootScope.openModal("app/components/content/administerLinkCategories/addEditLinkCategories/editLinkCategoryModal.html", "AddEditLinkCategoriesController");
     };
 
+    $scope.archivedLinkCategoriesButton = true;
+    $scope.linkCategoriesHeadingClass = "white-color blue-background";
+    $scope.archivedLinkCategoriesButtonClass = "btn btn-default pull-right";
+    $scope.changeLinkCategoriesClass = function () {
+        if ($scope.archivedLinkCategoriesButton == true){
+            $scope.linkCategoriesHeadingClass = "white-color darkgray-background";
+            $scope.archivedLinkCategoriesButtonClass = "btn btn-primary pull-right";
+            $scope.archivedLinkCategoriesButton = false;
+        } else {
+            $scope.linkCategoriesHeadingClass = "white-color blue-background";
+            $scope.archivedLinkCategoriesButtonClass = "btn btn-default pull-right";
+            $scope.archivedLinkCategoriesButton = true;
+        }
+    };
+    $scope.recoverLinkCategory = function (linkCategory) {
+        linkCategory.isArchived = 0;
+        $rootScope.notifySuccess("Link-kategorien ble gjenopprettet!", 1000);
+    };
+
 }]);
